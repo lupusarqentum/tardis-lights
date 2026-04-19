aa880d478ec4be1b88b9b8dad10f433cb1150774
Grigoriy Loboda <lupusarqentum@protonmail.com>
2026-04-19 16:42:41 +0300

    feat(wasm): connect C code to JS through Wasm

---
+++ src/hal_wasm.c
@@ -0,0 +1,34 @@
+#include "hal.h"
+#include "main_loop.h"
+#include "puzzle.h"
+#include "libc_subset.h"
+
+extern int jsReadSwitch(int);
+extern void jsSetLightState(int, int);
+extern void jsAddLights(int);
+extern void jsAddSwitches(int);
+extern void jsLog(int, int);
+
+bool hal_read(int switch_number) {
+    return jsReadSwitch(switch_number) == 1;
+}
+
+void hal_write(int light_number, bool value) {
+    jsSetLightState(light_number, value ? 1 : 0);
+}
+
+void hal_log(const char* message) {
+    jsLog((int)message, strlen(message));
+}
+
+__attribute__((export_name("setup")))
+void setup(void) {
+    jsAddLights(LIGHT_COUNT);
+    jsAddSwitches(SWITCH_COUNT);
+    main_loop();
+}
+
+__attribute__((export_name("loop")))
+void loop(void) {
+    main_loop();
+}
