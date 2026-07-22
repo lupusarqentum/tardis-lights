f717e5ae5f34a12777a300368dbd304308c374ee
Grigoriy Loboda <lupusarqentum@protonmail.com>
2026-07-23 01:51:08 +0300

    refactor: remove logging capabilities
    remove logging capabilities for now in order to simplify project.

--- include/hal.h
+++ include/hal.h
@@ -8,7 +8,6 @@ extern "C" {
 
 tl_bool hal_read(int switch_number);
 void hal_write(int light_number, tl_bool value);
-void hal_log(const char* message);
 
 #ifdef __cplusplus
 }
--- src/main_loop.c
+++ src/main_loop.c
@@ -1,4 +1,3 @@
-#include "libc_subset.h"
 #include "hal.h"
 #include "puzzle.h"
 
@@ -9,16 +8,6 @@ static void input(void) {
     for (int i = 0; i < SWITCH_COUNT; ++i) {
         switch_state[i] = hal_read(i);
     }
-
-    const char message_start[] = "Switch state just read: ";
-    char buffer[sizeof(message_start) - 1 + SWITCH_COUNT + 2];
-    tl_memcpy(buffer, message_start, sizeof(message_start) - 1);
-    for (int i = 0; i < SWITCH_COUNT; ++i) {
-        buffer[sizeof(message_start) - 1 + i] = switch_state[i] ? '1' : '0';
-    }
-    buffer[sizeof(message_start) - 1 + SWITCH_COUNT + 0] = '\n';
-    buffer[sizeof(message_start) - 1 + SWITCH_COUNT + 1] = '\0';
-    hal_log(buffer);
 }
 
 static void logic(void) {
@@ -29,16 +18,6 @@ static void render(void) {
     for (int i = 0; i < LIGHT_COUNT; ++i) {
         hal_write(i, light_state[i]);
     }
-
-    const char message_start[] = "Light state just rendered: ";
-    char buffer[sizeof(message_start) - 1 + LIGHT_COUNT + 2];
-    tl_memcpy(buffer, message_start, sizeof(message_start) - 1);
-    for (int i = 0; i < LIGHT_COUNT; ++i) {
-        buffer[sizeof(message_start) - 1 + i] = light_state[i] ? '1' : '0';
-    }
-    buffer[sizeof(message_start) - 1 + LIGHT_COUNT + 0] = '\n';
-    buffer[sizeof(message_start) - 1 + LIGHT_COUNT + 1] = '\0';
-    hal_log(buffer);
 }
 
 void main_loop(void) {
