d41126fc4008cb1f63b0b3ce33fd5e2d549bbd0b
Grigoriy Loboda <lupusarqentum@protonmail.com>
2026-07-23 17:29:03 +0300

    feat: add physical wiring test to be run at program restart

--- main.c
+++ main.c
@@ -4,9 +4,31 @@
 #include "hal.h"
 #include "puzzle.h"
 
+/* intended to run at the start to check if everything is wired correctly */
+void test_lights_wiring()
+{
+	/* blink all LEDs to check that they are wired */
+	for (unsigned char i = 4; i--;) {
+		hal_write(0xFF);
+		hal_delay(150);
+		hal_write(0);
+		hal_delay(150);
+	}
+
+	/* blink all LEDs separately to check that no LEDs are intermixed */
+	for (unsigned char i = 0; i < 8; i++) {
+		unsigned char light_state = 1 << i;
+
+		hal_write(light_state);
+		hal_delay(150);
+	}
+}
+
 int main()
 {
 	hal_setup();
+
+	test_lights_wiring();
 
 	while (1) {
 		unsigned char switch_state, light_state;
@@ -14,6 +36,6 @@ int main()
 		switch_state = hal_read();
 		light_state = puzzle_update(switch_state);
 		hal_write(light_state);
-		hal_delay(750);
+		hal_delay(10);
 	}
 }
