291d7ceaf47a87ca014dc955b34a4b76bb72e77e
Grigoriy Loboda <lupusarqentum@protonmail.com>
2026-07-23 17:17:21 +0300

    feat: update puzzle to v2 (so called beautiful version)

--- puzzle.c
+++ puzzle.c
@@ -3,33 +3,53 @@
 
 #include "puzzle.h"
 
-#define A7 switch_state[0]
-#define B7 switch_state[1]
-#define C7 switch_state[2]
-#define D7 switch_state[3]
-#define E7 switch_state[4]
+#define GET(v, i) (((v) >> (i)) & 1)
+#define SET(v, i, bit) ((v) |= (((bit) & 1) << (i)))
 
-#define NOT(x) ((x) == false)
+#define NOT(x) (!(x))
 #define AND(x, y) ((x) && (y))
 #define OR(x, y) ((x) || (y))
 #define XOR(x, y) ((x) ^ (y))
 
-unsigned char puzzle_update(unsigned char input)
+#define A7 (GET(switch_state, 0))
+#define B7 (GET(switch_state, 1))
+#define C7 (GET(switch_state, 2))
+#define D7 (GET(switch_state, 3))
+#define E7 (GET(switch_state, 4))
+
+#define A20 A7
+#define B20 B7
+#define C20 C7
+#define D20 D7
+#define E20 E7
+
+// unsigned char puzzle_update(unsigned char switch_state)
+// {
+// 	unsigned char result = 0;
+
+// 	SET(result, 0, AND(NOT(A7), B7));
+// 	SET(result, 1, AND(NOT(C7), NOT(D7)));
+// 	SET(result, 2, E7);
+// 	SET(result, 3, AND(B7, E7));
+// 	SET(result, 4, AND(NOT(A7), NOT(D7)));
+// 	SET(result, 5, XOR(B7, C7));
+
+// 	return result;
+// }
+
+unsigned char puzzle_update(unsigned char switch_state)
 {
-        unsigned char result = 0;
-        unsigned char switch_state[5];
-        unsigned char light_state[6];
+	unsigned char result = 0;
 
-        light_state[0] = AND(NOT(A7), B7);
-	light_state[1] = AND(NOT(C7), NOT(D7));
-	light_state[2] = E7;
-	light_state[3] = AND(B7, E7);
-	light_state[4] = AND(NOT(A7), NOT(D7));
-	light_state[5] = XOR(B7, C7);
+	if (!A20)
+		return 0;
 
-	for (int i = 0; i < 6; i++) {
-	        result |= light_state[i] << i;
-	}
+	SET(result, 0, AND(XOR(A20, B20), A20));
+	SET(result, 1, AND(XOR(D20, C20), A20));
+	SET(result, 2, AND(OR(XOR(E20, A20), XOR(B20, D20)), A20));
+	SET(result, 3, AND(OR(XOR(B20, E20), XOR(C20, D20)), A20));
+	SET(result, 4, AND(OR(XOR(A20, D20), XOR(B20, E20)), A20));
+	SET(result, 5, AND(OR(XOR(A20, C20), XOR(D20, E20)), A20));
 
 	return result;
 }
