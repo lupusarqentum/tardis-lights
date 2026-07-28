528174991a16ac6be3d8a98a1277cd5926cd1d4a
Grigoriy Loboda <lupusarqentum@protonmail.com>
2026-07-28 05:07:02 +0300

    refactor: puzzle.c
    1) remove the requirement to specify bit index in macros used by puzzle.c
    this slightly decreases code size (around 10 bytes less)
    done by replacing SET macros by PUT macros (see diffs for details)
    2) put formulae sets into distinct functions puzzle_formulae_v1 and puzzle_formulae_v2 and call them from puzzle_update
    this eliminates necessity to comment parts of code

--- puzzle.c
+++ puzzle.c
@@ -4,7 +4,9 @@
 #include "puzzle.h"
 
 #define GET(v, i) (((v) >> (i)) & 1)
-#define SET(v, i, bit) ((v) |= (((bit) & 1) << (i)))
+#define PUT(v, bit)                                                            \
+	(v) <<= 1;                                                             \
+	(v) |= (bit)
 
 #define NOT(x) (!(x))
 #define AND(x, y) ((x) && (y))
@@ -23,33 +25,39 @@
 #define D20 D7
 #define E20 E7
 
-// unsigned char puzzle_update(unsigned char switch_state)
-// {
-// 	unsigned char result = 0;
-
-// 	SET(result, 0, AND(NOT(A7), B7));
-// 	SET(result, 1, AND(NOT(C7), NOT(D7)));
-// 	SET(result, 2, E7);
-// 	SET(result, 3, AND(B7, E7));
-// 	SET(result, 4, AND(NOT(A7), NOT(D7)));
-// 	SET(result, 5, XOR(B7, C7));
-
-// 	return result;
-// }
-
-unsigned char puzzle_update(unsigned char switch_state)
+static inline unsigned char puzzle_formulae_v1(unsigned char switch_state)
 {
 	unsigned char result = 0;
 
+	PUT(result, XOR(B7, C7));
+	PUT(result, AND(NOT(A7), NOT(D7)));
+	PUT(result, AND(B7, E7));
+	PUT(result, E7);
+	PUT(result, AND(NOT(C7), NOT(D7)));
+	PUT(result, AND(NOT(A7), B7));
+
+	return result;
+}
+
+static inline unsigned char puzzle_formulae_v2(unsigned char switch_state)
+{
+	unsigned char result = 0;
+
+	// the following branch helps compiler to produce code of less size
 	if (!A20)
 		return 0;
 
-	SET(result, 0, AND(XOR(A20, B20), A20));
-	SET(result, 1, AND(XOR(D20, C20), A20));
-	SET(result, 2, AND(OR(XOR(E20, A20), XOR(B20, D20)), A20));
-	SET(result, 3, AND(OR(XOR(B20, E20), XOR(C20, D20)), A20));
-	SET(result, 4, AND(OR(XOR(A20, D20), XOR(B20, E20)), A20));
-	SET(result, 5, AND(OR(XOR(A20, C20), XOR(D20, E20)), A20));
+	PUT(result, AND(OR(XOR(A20, C20), XOR(D20, E20)), A20));
+	PUT(result, AND(OR(XOR(A20, D20), XOR(B20, E20)), A20));
+	PUT(result, AND(OR(XOR(B20, E20), XOR(C20, D20)), A20));
+	PUT(result, AND(OR(XOR(E20, A20), XOR(B20, D20)), A20));
+	PUT(result, AND(XOR(D20, C20), A20));
+	PUT(result, AND(XOR(A20, B20), A20));
 
 	return result;
+}
+
+unsigned char puzzle_update(unsigned char switch_state)
+{
+	return puzzle_formulae_v2(switch_state);
 }
