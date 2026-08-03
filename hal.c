4fc55e38fbd133b0aba9eac54f192a9a7de646cf
Grigoriy Loboda <lupusarqentum@protonmail.com>
2026-08-03 23:02:38 +0300

    feat: send printed characters over hardware usart0

--- hal.c
+++ hal.c
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: 0BSD
 // Copyright (C) 2026 Grigoriy Loboda
 
-#include "avr/io.h"
-#include "util/delay.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
 
+#include "print.h"
 #include "hal.h"
+
+#ifndef BAUD
+#define BAUD 9600
+#endif
+#include <util/setbaud.h>
 
 void hal_setup(void)
 {
@@ -14,14 +21,38 @@ void hal_setup(void)
 	//         PC0, PC1, PC2, PC3, PC4, PC5
 	// for an additional external pull up D12 (PB4) is driven high
 
+	// switches
+
+	DDRD &= ~0x7C;
+	PORTD |= 0x7C;
+
+	// lights
+
 	DDRC |= 0x3F;
 	PORTC &= ~0x3F;
+
+	// external pull up PB4
 
 	DDRB |= (1 << 4);
 	PORTB |= (1 << 4);
 
-	DDRD &= ~0x7C;
-	PORTD |= 0x7C;
+	cli();
+
+	// USART0 is used for printing (print.h)
+	// params: 9600baud, 8data, no parity
+
+	UBRR0H = UBRRH_VALUE; // defined by <util/setbaud.h>
+	UBRR0L = UBRRL_VALUE;
+#if USE_2X // defined by <util/setbaud.h>
+	UCSR0A |= (1 << U2X0);
+#else
+	UCSR0A &= ~(1 << U2X0);
+#endif
+	UCSR0B = (1 << TXEN0);			   // transmitter enabled
+	UCSR0C = (0 << UMSEL00) | (0 << UMSEL01) | // asynchronous mode
+		 (1 << UCSZ01) | (1 << UCSZ00);	   // 8data
+
+	sei();
 }
 
 void hal_delay(unsigned char milli)
@@ -33,6 +64,7 @@ void hal_delay(unsigned char milli)
 unsigned char hal_read(void)
 {
 	unsigned char result = PIND;
+
 	// note ~ operator; it is here because of pull up resistors being used
 	result = (~(result >> 2)) & 0x1F;
 	return result;
@@ -41,4 +73,22 @@ unsigned char hal_read(void)
 void hal_write(unsigned char light_state)
 {
 	PORTC = (PORTC & ~0x3F) | (light_state & 0x3F);
+}
+
+void hal_start_print_buffer_transmission(void)
+{
+	// enable UDRE interrupt
+	UCSR0B |= (1 << UDRIE0);
+}
+
+ISR(USART_UDRE_vect)
+{
+	if (!pr_empty()) {
+		unsigned char next = pr_getch();
+
+		UDR0 = next;
+	} else {
+		// disable UDRE interrupt
+		UCSR0B &= ~(1 << UDRIE0);
+	}
 }
