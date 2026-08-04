0849130b6cc3a5ff44912c67c971627e158bcfe6
Grigoriy Loboda <lupusarqentum@protonmail.com>
2026-08-04 10:24:15 +0300

    refactor: change logs format

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: 0BSD
 // Copyright (C) 2026 Grigoriy Loboda
 
+#include <avr/io.h>
 #include <stdint.h>
 
 #include "hal.h"
@@ -27,25 +28,22 @@ static void test_lights_wiring(void)
 	}
 }
 
-static void log_message(uint16_t time, const char* msg, unsigned char binary)
+static void log_states(unsigned char time, unsigned char switches, unsigned char lights)
 {
-	unsigned char low = time & 0xFF;
-	unsigned char high = (time >> 8) & 0xFF;
-
 	pr_string("0x");
-	pr_hex(high);
-	pr_hex(low);
-	pr_string(": ");
-	pr_string(msg);
-	pr_binary(binary);
+	pr_hex(time);
+	pr_string(" ");
+	pr_string("New state detected SW: ");
+	pr_binary(switches);
+	pr_string(" LED: ");
+	pr_binary(lights);
 	pr_char('\n');
 }
 
 int main(void)
 {
-	uint16_t iteration_number = 0;
 	unsigned char prev_switch, prev_light;
-	unsigned char first_iteration = 1;
+	unsigned char input_changes_count = 0;
 
 	hal_setup();
 
@@ -55,22 +53,18 @@ int main(void)
 		unsigned char switch_state, light_state;
 
 		switch_state = hal_read();
-		if (first_iteration || switch_state != prev_switch) {
+		if (input_changes_count == 0 || switch_state != prev_switch) {
+			input_changes_count++;
 			light_state = puzzle_update(switch_state);
 			hal_write(light_state);
-			log_message(iteration_number,
-				    "New switches state: ", switch_state);
-			log_message(iteration_number,
-				    "New lights state:   ", light_state);
+			log_states(input_changes_count, switch_state, light_state);
 			hal_start_print_buffer_transmission();
 		} else {
 			light_state = prev_light;
 		}
 
-		iteration_number++;
 		prev_light = light_state;
 		prev_switch = switch_state;
-		first_iteration = 0;
 
 		hal_delay(10);
 	}
